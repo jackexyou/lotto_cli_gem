ad5414604c1c47ce3cda6bebc87e80dd1823c3b3
Jack <github email address>
2017-06-26 11:53:46 -0400

    Finished all Scraper methods

--- lib/lotto_cli_gem/scraper.rb
+++ lib/lotto_cli_gem/scraper.rb
@@ -8,22 +8,24 @@ class LottoCliGem::Scraper
     site = Nokogiri::HTML(open("http://lottery.olg.ca/en-ca/"))
     game_urls = site.search("div.col-1 a.games-menu-item").collect {|game| game.attr("href")}
     game_urls.pop #removes encore from the list
+    game_urls
   end
 
-  def scrape_all(game_urls)
-    game_urls.each do |game_url|
-      
-    end
-  end
 
   def get_winning_number(game_url)
     #scrape name
     site = Nokogiri::HTML(open(game_url))
     name = site.search("title").text.chomp(" | OLG")
-    binding.pry
     #scrape winning numbers
     winning_numbers = site.search("ul.winning-number span.number").collect {|x| x.text}
     #Game.create_game method
+    binding.pry
+  end
+
+  def scrape_all(game_urls)
+    game_urls.each do |game_url|
+      get_winning_number(game_url)
+    end
   end
 
 end
